83e67f842c15dcc1384488e688af8ada4d22f060
Corey O'Connor <coreyoconnor@gmail.com>
2011-11-16 19:53:14 -0800

    nevermind! packed int representations should not be part of core

--- src/Bind/Marshal/StdLib.hs
+++ src/Bind/Marshal/StdLib.hs
@@ -5,7 +5,6 @@ module Bind.Marshal.StdLib ( module Bind.Marshal.StdLib
                            , module Bind.Marshal.StdLib.Des
                            , module Bind.Marshal.StdLib.Dynamic.FixedBuffer
                            , module Bind.Marshal.StdLib.Dynamic.ByteString.Lazy
-                           , module Bind.Marshal.StdLib.Packed
                            , module Bind.Marshal.StdLib.Ser
                            , module Bind.Marshal.StdLib.Types
                            , module Bind.Marshal.StdLib.Utils
@@ -17,7 +16,6 @@ import Bind.Marshal.Prelude
 import Bind.Marshal.StdLib.Des
 import Bind.Marshal.StdLib.Dynamic.ByteString.Lazy
 import Bind.Marshal.StdLib.Dynamic.FixedBuffer
-import Bind.Marshal.StdLib.Packed
 import Bind.Marshal.StdLib.Ser
 import Bind.Marshal.StdLib.Types
 import Bind.Marshal.StdLib.Utils
